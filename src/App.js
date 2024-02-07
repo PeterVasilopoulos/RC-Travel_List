3f36c7fe90a6306db536cb61d31fcb306e823ab1
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-07 16:49:24 -0500

    finished stats section

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
                 onToggleItem={handleToggleItem} 
             />
 
-            <Stats />
+            <Stats items={items} />
         </div>
     )
 }
@@ -115,10 +115,31 @@ function Item({item, onDeleteItem, onToggleItem}) {
     )
 }
 
-function Stats() {
+function Stats({items}) {
+    if(!items.length) {
+        return (
+            <p className='stats'>
+                <em>
+                    Start adding some items to your packing list 🚀
+                </em>
+            </p>
+        )
+    }
+
+    const numItems = items.length
+    const numPacked = items.filter((item) => item.packed).length
+    const percentPacked = Math.round(numPacked / numItems * 100)
+
     return (
     <footer className='stats'>
-        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+        <em>
+            {percentPacked !== 100 ? 
+            `💼 You have ${numItems} item${numItems > 1 ? 's ' : ' '}
+            on your list, and you already packed ${numPacked} (${percentPacked}%)`
+            :
+            'You got everything! Ready to go ✈'
+        }
+        </em>
     </footer>
     )
 }
